090374ca05be38547a090009063dbfce5aa19896
Somsong <punkungpunkub01@gmail.com>
2024-02-09 22:08:41 +0700

    get products all+id
    create path getproduct by id

--- src/main/java/com/example/POS/Controller/MainController.java
+++ src/main/java/com/example/POS/Controller/MainController.java
@@ -3,9 +3,12 @@ package com.example.POS.Controller;
 import com.example.POS.Models.Products;
 import com.example.POS.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class MainController {
@@ -16,6 +19,25 @@ public class MainController {
     @PostMapping("/addProduct")
     public void addProduct(@RequestBody Products products){
         productRepository.save(products);
+    }
+    @GetMapping("/products")
+    public ResponseEntity<?> getAllProducts(){
+        List<Products> products = productRepository.findAll();
+        if (!products.isEmpty()){
+            return new ResponseEntity<List<Products>>(products, HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>("No products available", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<?> getProductsById(@PathVariable("id") String id){
+        Optional<Products> products = productRepository.findById(id);
+        if (products.isPresent()){
+            return new ResponseEntity<>(products, HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>("Products not found", HttpStatus.NOT_FOUND);
+        }
     }
 
 }
