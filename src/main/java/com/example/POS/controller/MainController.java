2f232a7da0cdd366da6e3bbf0d737a1e2ee9a7a9
Somsong <punkungpunkub01@gmail.com>
2024-02-12 23:28:11 +0700

    attemp 01
    เอา data ลงในเว็บ

--- src/main/java/com/example/POS/controller/MainController.java
+++ src/main/java/com/example/POS/controller/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -74,6 +75,13 @@ public class MainController {
     @GetMapping("/")
     public String index(){
         return "main";
+    }
+    @ModelAttribute
+    @GetMapping("/sell")
+    public String showSellPage(Model model){
+        List<Products> products = productRepository.findAll();
+        model.addAttribute("Product:", products);
+        return "sell";
     }
 
 }
