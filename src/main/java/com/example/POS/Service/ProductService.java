3f3c5dffabcd5d1ac8f784f7c6e6f084a8995635
Somsong <punkungpunkub01@gmail.com>
2024-02-16 22:13:52 +0700

    add createProduct
    for Service layer

--- src/main/java/com/example/POS/Service/ProductService.java
+++ src/main/java/com/example/POS/Service/ProductService.java
@@ -1,7 +1,11 @@
 package com.example.POS.Service;
 
 import com.example.POS.Models.Products;
+import com.example.POS.exception.BaseException;
 
 public interface ProductService {
     String save(Products products);
+
+    void createProduct(Products products) throws BaseException;
+
 }
--- src/main/java/com/example/POS/Service/ProductServiceImp.java
+++ src/main/java/com/example/POS/Service/ProductServiceImp.java
@@ -2,8 +2,11 @@ package com.example.POS.Service;
 
 import com.example.POS.Models.Products;
 import com.example.POS.Repository.ProductRepository;
+import com.example.POS.exception.BaseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProductServiceImp implements ProductService {
@@ -15,5 +18,20 @@ public class ProductServiceImp implements ProductService {
         return productRepository.save(products).getId();
     }
 
+    @Override
+    public void createProduct(Products products) throws BaseException {
+        Optional<Products> productsOptional = productRepository.findByProduct(products.getName());
+        if (productsOptional.isPresent()){
+            System.out.println("Same");
+            throw new BaseException(BaseException.ProductAlreadyExists());
+        }
+        if (products.getName().isBlank()) {
+            System.out.println("Name nulla");
+            throw new BaseException(BaseException.ProductNameNull());
+        }else {
+            System.out.println("save dai");
+            save(products);
+        }
+    }
 
 }
