a75fdb6f5c2fa7d02cad6f359dda6ddac2d1b5e0
Somsong <punkungpunkub01@gmail.com>
2024-02-19 22:57:26 +0700

    Create SellProducts model

---
+++ src/main/java/com/example/POS/Models/SellProducts.java
@@ -0,0 +1,19 @@
+package com.example.POS.Models;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document("sellproduct")
+public class SellProducts {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private double price;
+
+    private int amount;
+}
