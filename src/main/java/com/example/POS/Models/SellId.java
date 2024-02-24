4e4c97bb809b8fa4bc67c994d06a77200442f241
Somsong <punkungpunkub01@gmail.com>
2024-02-24 21:39:28 +0700

    add sellrepo and sell model

---
+++ src/main/java/com/example/POS/Models/SellId.java
@@ -0,0 +1,15 @@
+package com.example.POS.Models;
+
+import lombok.Data;
+
+@Data
+public class SellId {
+
+    private String id;
+
+    private String name;
+
+    private double price;
+
+    private int amount;
+}
